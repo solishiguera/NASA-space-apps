3193ed079d6fff128dd2743a21efaaf935ab5f29
Diego <A00827847@itesm.mx>
2021-10-03 17:17:51 -0500

    Todos juntos

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-//Load HTTP module
-const http = require("http");
-const hostname = '127.0.0.1';
-const port = 3000;
+var http = require('http');
+var fs = require('fs');
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+}
 
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+http.createServer(onRequest).listen(8000);
